ac7c67f8284fdb2febe4047252f9d6cc76f6d9b9
Elamathi <ems112000@gmail.com>
2025-12-07 11:50:51 -0600

    updated allure automation

--- stepDefinitions/Login_Step.js
+++ stepDefinitions/Login_Step.js
@@ -1,70 +1,100 @@
-  import {createBdd} from 'playwright-bdd';
-  import { test } from '../fixtures/Index_Fixture.js';
-  import { expect } from '@playwright/test';
+import { createBdd } from 'playwright-bdd';
+import { test } from '../fixtures/Index_Fixture.js';
+import { expect } from '@playwright/test';
 
-  //const {Given, When, Then} = createBdd(test);
-  const { Given, When, Then } = createBdd(test, { passTags: true });
+//const {Given, When, Then} = createBdd(test);
+const { Given, When, Then } = createBdd(test, { passTags: true });
 
-Given('User is on SweetBalance homepage', async ({pages}) => {
+Given('User is on SweetBalance homepage', async ({ pages }) => {
   await pages.login.launchApp();
 });
 
-When('User clicks on Login link', async ({pages}) => {
-  
-    await pages.login.loginBtn.click();
+
+When('User clicks on Login link', async ({ pages }) => {
+
+  await pages.login.loginBtn.click();
 });
 
-Then('User should see {string} heading in home page', async ({pages}, expectedText) => {
- const actualText = await pages.login.getModalText();
- console.log("actualText :" +"--->"+actualText);
+Then('User should see {string} heading in home page', async ({ pages }, expectedText) => {
+
+  const actualText = await pages.login.getModalText();
+  console.log("actualText :" + "--->" + actualText);
   await expect(actualText).toContain(expectedText);
 });
 
-Then('User should see {string} with {string} in home page', async ({pages}, scenario, expected) => {
+Then('User should see {string} with {string} in home page', async ({ pages }, scenario, expected) => {
   switch (scenario.toLowerCase().trim()) {
     case "presence of close button":
       await expect(pages.login.closeBtn).toBeVisible();
       break;
     case "email input field":
       await expect(pages.login.emailTextBox).toBeVisible();
-      break;  
+      break;
     case "placeholder text in email field":
-      const actual = await pages.login.emailTextBox.getAttribute('placeholder'); 
+      const actual = await pages.login.emailTextBox.getAttribute('placeholder');
       expect(actual.trim()).toBe(expected.trim());
       break;
     case "Continue with Email is visible":
       await expect(pages.login.contWithEmailBtn).toBeVisible();
-      break;  
+      break;
     case "Continue with Email is enabled":
       await expect(pages.login.contWithEmailBtn).toBeEnabled();
-      break;    
+      break;
     case "Continue with Google is visible":
       await expect(pages.login.contWithGoogleBtn).toBeVisible();
-      break;   
+      break;
   }
- 
-});          
 
-Given('User is on the login page', async ({pages}) => {
+});
+
+Given('User is on the login page', async ({ pages }) => {
   await pages.login.loginBtn.click();
 });
 
-When('User clicks continue with email button after entering email', async ({pages,testData}) => {
+When('User clicks continue with email button after entering email', async ({ pages, testData }) => {
   await pages.login.fillEmailAndClickCont(testData.Email);
- 
+
 });
 
-Then('User should see {string} with {string} for login', async ({}, scenario, expected) => {
- 
+//Module 2
+
+Then('User should see {string} with {string} for login', async ({ pages, testData }, scenario, expected) => {
+  switch (scenario.toLowerCase().trim()) {
+    case "email input accepts valid existing email":
+      await expect(pages.login.passwordTextBox).toBeVisible();
+      break;
+    case "email input rejects invalid email format":
+      const actualErrorMsg = await pages.login.validateErrorMsg();
+      const expectedErrorMsg = testData.Expected;
+      await expect(actualErrorMsg).toContain(expectedErrorMsg);
+      break;
+    case "presence of signin button":
+      await expect(pages.login.signinBtn).toBeVisible();
+      break;
+    case "sub text in login":
+      const actualEmail = await pages.login.getReflectedEmailText();
+      const expectedEmail = testData.Email;
+      await expect(actualEmail).toBe(expectedEmail);
+      break;
+    case "presence of forgot password link":
+      await expect(pages.login.contWithEmailBtn).toBeEnabled();
+      break;
+    case "presence of placeholder in password field":
+      const actualPlaceholder = await pages.login.getPwdPlaceholderText();
+      const expectedPlaceholder = testData.Expected;
+      await expect(actualPlaceholder).toBe(expectedPlaceholder);
+      break;
+  }
+
 });
 
 // Testcase: Login_19
-When('User Registered user clicks sign in after entering password', async ({pages}) => {
+When('User Registered user clicks sign in after entering password', async ({ pages }) => {
   // Step: When User Registered user clicks sign in after entering password
   // From: featureFiles\Login.feature:58:5
 });
 
-Then('User should be navigated to home page', async ({pages}) => {
+Then('User should be navigated to home page', async ({ pages }) => {
   // Step: Then User should be navigated to home page
   // From: featureFiles\Login.feature:59:5
 });
