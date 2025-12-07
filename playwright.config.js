90f4fa984d6268e774ac3ffcbc81293aca934d10
Elamathi <ems112000@gmail.com>
2025-12-07 15:52:12 -0600

    Second update for CI/CD run

--- playwright.config.js
+++ playwright.config.js
@@ -1,68 +1,73 @@
-  // @ts-check
-  import { defineConfig, devices } from '@playwright/test';
-  import {defineBddConfig} from 'playwright-bdd';
-  import dotenv from 'dotenv';
-  import path from 'path';
-  
-  
- // dotenv.config({ path: path.resolve(__dirname, 'SweetBalance.env') }); //runs only in local not in CI/CD
- dotenv.config({ path: path.resolve(process.cwd(), 'SweetBalance.env') });
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+import { defineBddConfig } from 'playwright-bdd';
+import fs from 'fs';
+import dotenv from 'dotenv';
+import path from 'path';
 
-  
-  const testDir = defineBddConfig({
-    features: ['featureFiles/**.feature'],
-    steps: [
-     
-      'stepDefinitions/**.js',
-      'fixtures/Index_Fixture.js'],
-      
 
-  }); 
-      
-  export default defineConfig({
-    //testDir: './tests',
-    testDir,
-     retries: 3,
-     globalSetup: "./global-setup.js",
-     globalTeardown: './global-teardown.js',
+// dotenv.config({ path: path.resolve(__dirname, 'SweetBalance.env') }); //runs only in local not in CI/CD
+//dotenv.config({ path: path.resolve(process.cwd(), 'SweetBalance.env') });
 
-    fullyParallel: true,
-    forbidOnly: !!process.env.CI,
-    /* Retry on CI only */
-    //retries: process.env.CI ? 2 : 0,
-    /* Opt out of parallel tests on CI. */  
-    workers: process.env.CI ? 1 : undefined,
+const envPath = process.env.ENV_PATH || path.resolve(process.cwd(), 'SweetBalance.env');
+dotenv.config({ path: envPath });
+console.log(`[Config] Using .env file at: ${envPath}`);
 
-   reporter: [['html'],
-             ['allure-playwright',{outputFolder:'Allure-Report'}]
 
-],
+const testDir = defineBddConfig({
+  features: ['featureFiles/**.feature'],
+  steps: [
 
-    use: {
+    'stepDefinitions/**.js',
+    'fixtures/Index_Fixture.js'],
 
-     // baseURL: process.env.BASE_URL, 
-      trace: 'on-first-retry',
+
+});
+
+export default defineConfig({
+  //testDir: './tests',
+  testDir,
+  retries: 3,
+  globalSetup: "./global-setup.js",
+  globalTeardown: './global-teardown.js',
+
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  //retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+
+  reporter: [['html'],
+  ['allure-playwright', { outputFolder: 'Allure-Report' }]
+
+  ],
+
+  use: {
+
+    // baseURL: process.env.BASE_URL, 
+    trace: 'on-first-retry',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
     },
 
-    /* Configure projects for major browsers */
-    projects: [
-      {
-        name: 'chromium',
-        use: { ...devices['Desktop Chrome'] },
-      },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
 
-      {
-        name: 'firefox',
-        use: { ...devices['Desktop Firefox'] },
-      },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
 
-      {
-        name: 'webkit',
-        use: { ...devices['Desktop Safari'] },
-      },
 
-     
-    ],
+  ],
 
-  });
+});
 
