d7821400ee84a762bdb18aab51d2ec74ddbee255
Padmini <padmini1991@gmail.com>
2025-12-10 17:27:27 -0600

    Premium User Exercise Step Execution

--- pages/PremiumUsrExercise_Page.js
+++ pages/PremiumUsrExercise_Page.js
@@ -18,6 +18,8 @@ export class PremiumUsrExercise_Page{
         this.exerciseTime = page.locator('//div[@class="flex space-x-4 text-sm text-gray-500"]/span[1]');
         this.exerciseCalorie = page.locator('//div[@class="flex space-x-4 text-sm text-gray-500"]/span[2]');
         this.exerciseIntensity = page.locator('//div[@class="flex space-x-4 text-sm text-gray-500"]/span[3]');
+        this.successmsg = page.getByText('Success!', { exact: true });
+        this.wrkCompmsg = page.getByText('Warm Up completed', { exact: true });
 
         //Today's Exercise Schedule Page
         this.exeSchdTitle = page.getByRole('heading', { name: 'Today\'s Exercise Schedule' });
@@ -38,15 +40,49 @@ export class PremiumUsrExercise_Page{
         this.mainWrkIntens = page.locator('//h2[text()="Main Workout"]/following-sibling::div//span[3]');
 
         this.coolDwnSection = page.getByRole('heading', { name: 'Cool Down' });
-        this.coolDwnExerName = page.locator('//div/h2[text()="Main Workout"]/following::h3[1]');
-        this.coolDwnExerDesc = page.locator('//div/h2[text()="Main Workout"]/following::p[1]');
-        this.coolDwnExerTime = page.locator('//h2[text()="Main Workout"]/following-sibling::div//span[1]');
-        this.coolDwnExerCalo = page.locator('//h2[text()="Main Workout"]/following-sibling::div//span[2]');
-        this.coolDwnIntens = page.locator('//h2[text()="Main Workout"]/following-sibling::div//span[3]');
+        this.coolDwnExerName = page.locator('//div/h2[text()="Cool Down"]/following::h3[1]');
+        this.coolDwnExerDesc = page.locator('//div/h2[text()="Cool Down"]/following::p[1]');
+        this.coolDwnExerTime = page.locator('//h2[text()="Cool Down"]/following-sibling::div//span[1]');
+        this.coolDwnExerCalo = page.locator('//h2[text()="Cool Down"]/following-sibling::div//span[2]');
+        this.coolDwnIntens = page.locator('//h2[text()="Cool Down"]/following-sibling::div//span[3]');
     
         this.totalDuration = page.locator('//div[@class="flex justify-between text-violet-800"]/span[1]');
         this.totalCal = page.locator('//div[@class="flex justify-between text-violet-800"]/span[2]');
 
     }
+
+    async getElementPositions() {
+        await this.fullSchdBtn.waitFor({ state: 'visible' });
+        await this.excSectionTitle.waitFor({ state: 'visible' });
+
+        const ViewFullSchdBtn = await this.fullSchdBtn.evaluate(e => e.getBoundingClientRect().x);
+        const SectionTitle = await this.excSectionTitle.evaluate(e => e.getBoundingClientRect().x);
+
+        return { ViewFullSchdBtn, SectionTitle };
+    }
+
+    async exerciseSection(){
+        await this.exerciseTab.click();
+    }
+
+    async clickundo() {
+        if (await this.markComplBtn.isVisible()) {
+            await this.markComplBtn.click();
+            await this.undoBtn.waitFor({ state: 'visible', timeout: 5000 });
+        }
+        await this.undoBtn.click();
+    }
+
+    async getValue(locator) {
+        const text = await locator.textContent();        
+        return text.split(':')[1].trim().toLowerCase();
+    }
+
+    async getTimeValue(locator) {
+        const text = await locator.textContent();
+        const match = text.match(/\d+/);
+        return match ? parseInt(match[0], 10) : 0;
+    }
+
 
 }
